5b2263c4c33fc371407eb887abb1227d1de7e877
José Armando Tun <tunjose@umes.edu.gt>
2025-09-14 12:10:02 -0600

    chore: deploy dist files directly to main branch

--- trabajo-sociedad-cansancio/vite.config.js
+++ trabajo-sociedad-cansancio/vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/'
+  base: '/Filosofia-Social/', // 👈 ¡ES CRUCIAL!
+  build: {
+    outDir: 'dist',
+  },
 });
