ceba5494a2a580a64bf65c6063ee94a1072cefad
José Armando Tun <tunjose@umes.edu.gt>
2025-09-14 12:30:53 -0600

    Refactor project configuration and add Tailwind CSS support
    - Updated index.html to include main.jsx script instead of previous assets.
    - Modified package.json to add homepage, predeploy, and deploy scripts for GitHub Pages.
    - Changed Vite base path to root for deployment compatibility.
    - Added Tailwind CSS configuration file with custom theme and animations.

--- trabajo-sociedad-cansancio/vite.config.js
+++ trabajo-sociedad-cansancio/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Filosofia-Social/', // 👈 ¡ES CRUCIAL!
+  base: '/', // 👈 ¡ES CRUCIAL!
   build: {
     outDir: 'dist',
   },
